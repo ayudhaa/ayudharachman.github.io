7391b0ceac48a0804680728fa8a90910edf6f8a6
ayudharachman <ayudharachman@gmail.com>
2020-09-30 15:30:00 +0700

    Update
    update less js and prayer-times html
    
    Co-Authored-By: ayudharachman <39934679+ayudharachman@users.noreply.github.com>

---
+++ assets/less.js
@@ -0,0 +1,9 @@
+function index() {
+    let app         = document.getElementById('typedtext');
+    let h3          = document.createElement('h3');
+    h3.innerHTML    = 'Prayer times';
+
+    app.appendChild(h3);
+}
+
+index();
