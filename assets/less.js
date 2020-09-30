9c8ac7a66daf86e86fbc5f3f8d7e3802abdf3d70
ayudharachman <ayudharachman@gmail.com>
2020-09-30 15:44:25 +0700

    Update less.js
    adding geolocation
    
    Co-Authored-By: ayudharachman <39934679+ayudharachman@users.noreply.github.com>

--- assets/less.js
+++ assets/less.js
@@ -1,9 +1,26 @@
+function success(position) {
+    console.log(position);
+}
+
+function error() {
+    alert('Sorry');
+}
+
+function userLocation() {
+    if (!navigator.geolocation) {
+        alert('Geolocation tidak didukung pada browser Anda, silahkan gunakan browser lain :)');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index() {
     let app         = document.getElementById('typedtext');
     let h3          = document.createElement('h3');
     h3.innerHTML    = 'Prayer times';
 
     app.appendChild(h3);
+    userLocation();
 }
 
 index();
