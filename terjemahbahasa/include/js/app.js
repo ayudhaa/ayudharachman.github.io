4a8d0eccfa65f40eec12d862bdcf112e0c9039de
ayudharachman <ayudharachman@gmail.com>
2022-05-10 16:29:11 +0700

    update app.js
    update javascript

--- terjemahbahasa/include/js/app.js
+++ terjemahbahasa/include/js/app.js
@@ -2,15 +2,27 @@ $("#form").on("submit", function(e){
     e.preventDefault();
     var teks = $('[name="enterteks"]').val();
     var tolang = $('[name="tolang"]').val();
-    if (tolang == 1) {var convertto = 'id';} else {var convertto = 'en';}
-    var settings = {
-        "url": 'https://amm-api-translate.herokuapp.com/translate?engine=google&text='+teks+'&to='+convertto+'',
-        "method": "GET",
-        "timeout": 0,
-    };
-    // console.log(settings);
-    $.ajax(settings).done(function (response) {
-        $("#result").html(response.data.result);
-        $("#form")[0].reset();
-    });
+    if (teks.length > 0) {
+        if (tolang == 1) {var convertto = 'id';} else {var convertto = 'en';}
+        var settings = {
+            "url": 'https://amm-api-translate.herokuapp.com/translate?engine=google&text='+teks+'&to='+convertto+'',
+            "method": "GET",
+            "timeout": 0,
+        };
+        // console.log(settings);
+        $.ajax({
+            beforeSend: function() {$("#submit").attr({value: 'Tunggu...', disabled: 'true'});}
+        })
+        $.ajax(settings).done(function (response) {
+            $("#result").html(response.data.result);
+            $("#form")[0].reset();
+            $("#submit").prop("disabled", false);
+            $("#submit").attr("value", 'Terjemahin!');
+        });
+    } else {
+        Swal.fire({
+            title: 'Hai,',
+            text: 'Tulis yang ingin di translate ya :)'
+        });
+    }
 });
