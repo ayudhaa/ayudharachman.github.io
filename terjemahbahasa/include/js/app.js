43f0127138defefabad4a1d3aa16eb88c58b44e9
ayudharachman <ayudharachman@gmail.com>
2022-05-18 15:22:52 +0700

    index.html, app.js
    update index dan js

--- terjemahbahasa/include/js/app.js
+++ terjemahbahasa/include/js/app.js
@@ -3,9 +3,8 @@ $("#form").on("submit", function(e){
     var teks = $('[name="enterteks"]').val();
     var tolang = $('[name="tolang"]').val();
     if (teks.length > 0) {
-        if (tolang == 1) {var convertto = 'id';} else {var convertto = 'en';}
         var settings = {
-            "url": 'https://api-translate.azharimm.site/translate?engine=google&text='+teks+'&to='+convertto+'',
+            "url": 'https://api-translate.azharimm.site/translate?engine=google&text='+teks+'&to='+tolang+'',
             "method": "GET",
             "timeout": 0,
         };
