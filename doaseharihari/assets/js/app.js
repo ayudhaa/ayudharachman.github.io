fed7a7fc722d19cbca14b14e0fca389192f1f905
ayudharachman <ayudharachman@gmail.com>
2022-05-24 09:51:27 +0700

    Update app.js

--- doaseharihari/assets/js/app.js
+++ doaseharihari/assets/js/app.js
@@ -1,11 +1,11 @@
 function sendRequest() {
     var input_ = $('[name="text_"]').val();
     var settings = {
-        "mode": "no-cors",
         "url": "https://doa-doa-api-ahmadramadhan.fly.dev/api/doa/pergi",
         "method": "GET",
-        "mode": "no-cors",
         "timeout": 0,
+        "Access-Control-Allow-Origin": "https://ayudharachman.github.io/doaseharihari",
+        "Content-Type": "application/json"
     };
     
     $.ajax(settings).done(function (response) {
